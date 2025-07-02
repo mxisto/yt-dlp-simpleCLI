3ba0a1870168e9e07e959e9f804ab30e3b99d673
MxEmexis <matheusxsk@gmail.com>
2025-07-02 19:17:22 -0300

    Squashed commit of the following:
    commit 69c083a098b63279f8909ed0f3bccc412e63d46c
    Author: MxEmexis <matheusxsk@gmail.com>
    Date:   Wed Jul 2 19:15:00 2025 -0300
    
        update and cleanup
    
        better organization regarding some processes, cleaned some redundancies

--- README.md
+++ README.md
@@ -14,8 +14,7 @@ Command-line utility to interact with yt-dlp
 
 The goal is to do a even simpler approach to interact with the yt-dlp binary via terminal.
 
-## To be used with the standalone yt-dlp binary
-This program is originally set to work with the standalone `yt-dlp` binary that is available [here](https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp). Since it is not recommended to use the binaries from your distro repo or pip as they generally are outdated (at least on Debian-based distros anyway), I generally put this file in `.local/share/applications/`, you can do something similar in Windows using [Cmder](https://cmder.app).
+This program is originally set to work with both the standalone `yt-dlp` binary that is available [here](https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp) or with the version from your package manager. Since it is not recommended to use the binaries from your distro repo or pip as they generally are outdated (at least on Debian-based distros anyway), you can put this file in `.local/share/applications/` in order for it to be called via shell, you can do something similar in Windows using [Cmder](https://cmder.app).
 
 ## Funcionalities
 
@@ -28,13 +27,13 @@ This program is originally set to work with the standalone `yt-dlp` binary that 
 - `shlex`
 - `os`
 
-## Packages needed
+## Packages needed in PATH
 
 - yt-dlp
 - mpv
 
 ## Build
-You can simply run the code via the python interpreter, because building a executable via `pyinstaller` may lead to some permission errors when downloading in some situations. However the build scripts are included for those who want to try.
+You can simply run the code via the python interpreter, because building a executable via `pyinstaller` may lead to some permission errors when downloading in some situations, since it runs shell commands in the end of the day. The build scripts are included for those who want to try.
 
 ***
 At the moment, the focus is working in Linux, tests on Windows have been few at least for the time being.
--- yt-dlp_simpleCLI.py
+++ yt-dlp_simpleCLI.py
@@ -11,6 +11,7 @@ browser_cookie_check = False
 metadata_check = False
 subs_check = False
 folder_check = False
+mpv_custom_flag = False
 
 video_link = str("")
 browser_name = str("")
@@ -22,32 +23,27 @@ cookies_flag = str(" --cookies from browser")
 folder_path_flag = str(" -P")
 
 ytdlp_bin = ""
+mpv_bin=""
+folder_path = ""
 user_name= ""
 
-# check for OS and change the yt-dlp binary name accordingly, also the username
+# check for OS and change the yt-dlp binary name and folder path accordingly, also the username
 system = platform.system()
 if system == "Linux":
     ytdlp_bin = "yt-dlp"
     user_name = subprocess.run(['whoami'], capture_output=True, text=True).stdout.strip()
+    folder_path = str(f"/home/{user_name}/Videos/")
     print ("System: Linux")
 elif system == "Windows":
     ytdlp_bin = "yt-dlp.exe"
     user_name = os.environ['USERNAME']
+    folder_path= str(fr"C:\Users\{user_name}\Videos")
     print ("System: Windows")
 
 download_combo = ""
 
-# setup for where the downloaded files go, it reads a .txt file named "yt-dlp-path.txt" and sets the path as the first line of the document
-try:
-    f = open('ytdlp-path.txt','x')
-except FileExistsError:
-    print("File for path read already created as ytdlp-path.txt")
-    folder_check = True
-with open('ytdlp-path.txt') as f:
-    folder_path = f.read()
-
 # cool logo :)
-print(r"""
+cool_logo=str(r"""
 __               __            ____    
 \ \       __  __/ /_      ____/ / /___ 
  \ \     / / / / __/_____/ __  / / __ \
@@ -55,10 +51,15 @@ __               __            ____
 /_/_____\__, /\__/      \__,_/_/ .___/ 
  /_____/____/      simple CLI /_/
  
- version 0.0.3   
+ version 0.0.4   
 """)
 
+print(cool_logo)
 print(f"Hello, {user_name}! What we are going to download today? :3")
+
+#______________________________________________________________________________
+
+# FUNCTIONS
 
 # process for URL download
 def set_combo():
@@ -95,29 +96,155 @@ def set_combo():
     # or corrupting the files in the download process
     os.system(a)
 
+# function to clear the screen, it is used in a lot of places in the code
+def clrscreen():
+    os.system('cls' if os.name == 'nt' else 'clear')  # clears the last operation from the shell
+
 # command for update the yt-dlp binary from inside the program
 def ytdlp_update():
 	os.system('yt-dlp -U')
+# selection of custom mpv frontend
+def mpv_sel():
+        global mpv_custom_flag, mpv_bin
+        
+        print("Are you using a mpv frontend? MPV is set as default.\nDo you want to set another one? (ex. Celluloid, Haruna, SMPlayer, etc...)")
+        a=input("Yes (y) - Use custom | No - Use default MPV (n): ")
+
+        try:
+            if a == "y":
+                mpv_bin=str(input("Type the name: "))
+                mpv_bin=mpv_bin.lower()
+                mpv_custom_flag = True
+    
+            elif a == "n":
+                if system == "Linux":
+                    mpv_bin = "mpv"
+                elif system == "Windows":
+                    mpv_bin = "mpv.exe"
+                    
+                mpv_custom_flag = False
+            mpv_play()
+        except NameError:
+            print("Invalid name. Check your options again!")
+        except ValueError:
+            print("Invalid option, try again...")
+            
 
 # command for streaming media using mpv
 def mpv_play():
-
-    if system == "Linux":
-        mpv_bin = "mpv"
-    elif system == "Windows":
-        mpv_bin = "mpv.exe"
+    global mpv_custom_flag, mpv_bin
 
     os.system('cls' if os.name == 'nt' else 'clear')
-    print("Stream online media via MPV")
+    print(f"Stream online media via {mpv_bin} | type m) to set a custom Mplayer")
     video_link=str(input("Insert URL: "))
-    mpv_run=str(f"{mpv_bin} {video_link}")
-    os.system(mpv_run)
-    input("Press Enter to continue...")
+    if video_link=="m":
+        mpv_sel()
+    else:
+        mpv_run=str(f"{mpv_bin} {video_link}")
+        print(mpv_run)
+        os.system(mpv_run)
+        input("Press Enter to continue...")
+
+# media format selection    
+def format_sel():
+    global format_sel, formato, format_name, subs_check, metadata_check, metadata_flag
+    
+    print("Please select the media format:")
+    print("1) mp4 | 2) mp3")
+    formato_sel = str(input(": "))
+    if formato_sel == "1":
+        print("Format set to Mp4")
+        formato = str(" -t mp4") # preset alias from yt-dlp
+        format_name = str("Mp4")
+        sub_check = str(input("save subtitles? (y) or (n)?"))
+        if sub_check == "y":
+            subs_check = True
+        elif sub_check == "n":
+            subs_check = False
+        else:
+            print("Invalid command, setting subtitles check to False")
+            subs_check = False
+    elif formato_sel == "2":
+        print("Format set to Mp3")
+        formato = str(" -t mp3")  # preset alias from yt-dlp
+        meta_check = str(input("save metadata? (y) or (n)?"))
+        if meta_check == "y":
+            metadata_check = True
+        elif meta_check == "n":
+            metadata_check = False
+        else:
+            print("Invalid command, setting metadata check to False")
+            metadata_check = False
+        format_name = str("Mp3")
+    else:
+        print("Invalid command, try again...")
+    clrscreen()
+
+# folder path selection - simpler approach    
+def path_sel():
+    global folder_check, folder_path, folder_path_flag, system
+    
+    path_confirm = str(input(f"do you want to use the default folder or set another?\nDefault: {folder_path}\n(d) for default, (y) for set a new one, (n) for none, save in the same path as executable\n: "))
+    if path_confirm == "y":
+        if system == "Linux":
+            print(f"Tip: you can use ~/[...] in place of /home/{user_name}/[...]")
+            
+        p=str(input("Please set the new folder path: "))
+        folder_path=p
+        folder_check = True
+        
+    elif path_confirm == "d":
+        if system == "Linux":
+            folder_path = str(f"/home/{user_name}/Videos/")
+        elif system == "Windows":
+            folder_path= str(fr"C:\Users\{user_name}\Videos")
+        folder_check = True
+        
+    elif path_confirm == "n":
+        folder_path=""
+        folder_check = False
+    else:
+        print("Not a recognized command.")
+        input("Press Enter to continue...")
+    clrscreen()
+
+# set browser for login cookies and acess to download restricted contend
+def browser_sel():
+    global browser_name, browser_cookie_check
+    browser_name = str(input("Select Browser name...\nSupported browsers are: brave, chrome, chromium, edge,\nfirefox, opera, safari, vivaldi, whale.\nYou must be logged in on the targeted website.\n:"))
+    browser_cookie_check = True
+    clrscreen()
+    
+# resets the configurations and flags
+def reset_sel():
+    print("1) browser cookies | 2) metadata | 3) subtitles\n4) mpv values")
+    reset_choice = str(input("Select which parameter to reset: "))
+    if reset_choice == "1":
+        browser_cookie_check = False
+    elif reset_choice == "2":
+        metadata_check = False
+    elif reset_choice == "3":
+        subs_check = False
+    elif reset_choice == "4":
+        mpv_sel()
+    else:
+        print("Invalid command.")
+    clrscreen()
+
+# help and informations about the program
+def helpinfo():
+    clrscreen()
+    print(cool_logo)
+    print("Command-line utility written in Python to interact with yt-dlp.")
+    print("Check me on Github: https://github.com/m-xisto/yt-dlp-simpleCLI")
+    print("----------------------------")
+    input("Press Enter to go back...")
+    clrscreen()
+
 #______________________________________________________________________________
 
-# main loop
+# MAIN LOOP
 while True:
-	
     print("----------------------------")
     print(f"Format: {format_name}")
     print(f"Mp3 Metadata: {metadata_check} | Video Subtitles: {subs_check}")
@@ -130,7 +257,7 @@ i) to insert URL    |   f) media format
 b) browser cookies  |   p) set folder path
 r) reset parameters |   c) clear screen
 u) update yt-dlp    |   m) stream media (mpv)
-q) quit
+h) help             |   q) quit
 ____________________________
 """)
     
@@ -138,88 +265,25 @@ ____________________________
 
     try:
         if comando == "i":
-            os.system('cls' if os.name == 'nt' else 'clear')  # clears the last operation from the shell
+            clrscreen()
             video_link=str(input("Insert URL: "))
             set_combo()
             input("Press Enter to continue...")
 
         elif comando == "f":
-            print("Please select the media format:")
-            print("1) mp4 | 2) mp3")
-            formato_sel = str(input(": "))
-            if formato_sel == "1":
-                print("Format set to Mp4")
-                formato = str(" -t mp4") # preset alias from yt-dlp
-                format_name = str("Mp4")
-                sub_check = str(input("save subtitles? (y) or (n)?"))
-                if sub_check == "y":
-                    subs_check = True
-                elif sub_check == "n":
-                    subs_check = False
-                else:
-                    print("Invalid command, setting subtitles check to False")
-                    subs_check = False
-            elif formato_sel == "2":
-                print("Format set to Mp3")
-                formato = str(" -t mp3")  # preset alias from yt-dlp
-                meta_check = str(input("save metadata? (y) or (n)?"))
-                if meta_check == "y":
-                    metadata_check = True
-                elif meta_check == "n":
-                    metadata_check = False
-                else:
-                    print("Invalid command, setting metadata check to False")
-                    metadata_check = False
-                format_name = str("Mp3")
-            else:
-                print("Invalid command, try again...")
-            os.system('cls' if os.name == 'nt' else 'clear')  # clears the last operation from the shell
+            format_sel()
 
         elif comando == "b":
-            browser_name = str(input("Select Browser name...\nSupported browsers are: brave, chrome, chromium, edge,\nfirefox, opera, safari, vivaldi, whale.\nYou must be logged in on the targeted website.\n:"))
-            browser_cookie_check = True
-            os.system('cls' if os.name == 'nt' else 'clear')  # clears the last operation from the shell
+            browser_sel()
 
         elif comando == "p":
-            path_confirm = str(input("do you want to use the default folder or set another? (default is $user/Videos)\n(d) for default, (y) for set a new one, (n) for none, save in the same path as executable\n: "))
-            if path_confirm == "y":
-                p=str(input("Please set the new folder path: "))
-                with open('ytdlp-path.txt','w') as f:
-                    f.write(p)
-                with open('ytdlp-path.txt') as f:
-                    folder_path = f.read()
-                folder_check = True
-            elif path_confirm == "d":
-                if system == "Linux":
-                    user_name_path = str("/home/")+str(user_name)+str("/Videos")
-                elif system == "Windows":
-                    user_name_path = str('C:\\Users\\')+str(user_name)+str("\\Videos")
-                with open('ytdlp-path.txt','w') as f:
-                    f.write(user_name_path)
-                with open('ytdlp-path.txt') as f:
-                    folder_path = f.read()
-                folder_check = True
-            elif path_confirm == "n":
-                folder_check = False
-            else:
-                print("Not a recognized command.")
-            os.system('cls' if os.name == 'nt' else 'clear')  # clears the last operation from the shell
+            path_sel()
 
         elif comando == "r":
-            print("1) browser cookies | 2) metadata | 3) subtitles")
-            reset_choice = str(input("Select which parameter to reset: "))
-            if reset_choice == "1":
-                browser_cookie_check = False
-            elif reset_choice == "2":
-                metadata_check = False
-            elif reset_choice == "3":
-                subs_check = False
-            else:
-                print("Invalid command.")
-            os.system('cls' if os.name == 'nt' else 'clear')  # clears the last operation from the shell
+            reset_sel()
 
         elif comando == "c":
-            os.system('cls' if os.name == 'nt' else 'clear')  # clears the last operation from the shell
+            clrscreen()
 
         elif comando == "m":
             mpv_play()
@@ -229,6 +293,9 @@ ____________________________
             input("Press Enter to continue...")
             os.system('cls' if os.name == 'nt' else 'clear')
 			
+        elif comando == "h":
+            helpinfo()
+        
         elif comando == "q":
             print("Bye bye! (^.^)/")
             break
